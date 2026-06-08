1c01737e7d4714c1be9561a4f50abb9f49031845
Edwin Rivera <gaedwin92@gmail.com>
2026-06-08 15:38:04 -0600

    initial configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+const SITE = process.env.SITE || "https://roedwin-afk.github.io";
+const BASE = process.env.BASE || "/landing_thermalinsulationpanels/";
+
+export default defineConfig({
+  site: SITE,
+  base: BASE,
+  output: "static",
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          es: "es-ES",
+        },
+      },
+      filter: (page) => !page.includes("api"),
+    }),
+  ],
+});
